c71dc7b3420dbb5f32ccfe1026544945e9aee8e4
Sean Perry <sean.hyatt.perry@gmail.com>
2024-02-26 16:11:15 -0800

    added scrolling stuff for outline tmr

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,11 +1,30 @@
 <script>
-  // Write your JS here, or import other files
+  import Scroller from "@sveltejs/svelte-scroller";
+  let count, index, offset, progress;
+
 </script>
 
 <main>
   <h1>Svelte template</h1>
 
-  <p>Write your HTML here</p>
+  <Scroller
+  top={0.0}
+  bottom={1}
+  threshold={0.5}
+  bind:count
+  bind:index
+  bind:offset
+  bind:progress
+>
+  <div class="background" slot="background">
+
+  </div>
+
+  <div class="foreground" slot="foreground">
+    <section>test</section>
+
+  </div>
+</Scroller>
 </main>
 
 <style>
